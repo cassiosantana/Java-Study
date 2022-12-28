7ebb068e120e646ed0a9273fa68acb14651ca80d
Cassio Santana <cassioo.roger@gmail.com>
2022-12-28 12:24:21 -0300

    Abstract class and Interface

---
+++ Interface/Shapes/entities/Circle.java
@@ -0,0 +1,30 @@
+package entities;
+
+import enums.Color;
+
+public class Circle extends AbstractShape {
+
+	public Double radius;
+
+	public Circle(Color color) {
+		super(color);
+	}
+	
+	public Circle(Color color, Double radius) {
+		super(color);
+		this.radius = radius;
+	}
+
+	public Double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(Double radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public double area() {
+		return Math.PI * radius * radius;
+	}
+}
