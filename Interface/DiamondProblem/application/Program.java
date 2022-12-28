a12f82c78e88a53fc9596d672df29e1b8221cb44
Cassio Santana <cassioo.roger@gmail.com>
2022-12-28 16:31:23 -0300

    Solving the diamond problem with interfaces

--- Interface/DiamondProblem/application/Program.java
+++ Interface/DiamondProblem/application/Program.java
@@ -1,20 +1,28 @@
 package application;
 
-import devices.Printer;
-import devices.Scanner;
+import devices.ComboDevice;
+import devices.ConcretePrinter;
+import devices.ConcreteScanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Printer p = new Printer("55444");
+		ConcretePrinter p = new ConcretePrinter("55444");
 		p.processDoc("My Letter");
 		p.print("My Letter");
 		
 		System.out.println("------------------------------------");
 		
-		Scanner s = new Scanner("66555");
+		ConcreteScanner s = new ConcreteScanner("66555");
 		s.processDoc("My Email");
 		System.out.println("Scan result: " + s.scan());
+		
+		System.out.println("------------------------------------");
+		
+		ComboDevice c = new ComboDevice("77666");
+		c.processDoc("My document");
+		c.print("My document");
+		System.out.println("Combo Device result: " + c.scan());
 	}
 }
