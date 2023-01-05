223b321f08a0e5f7d6c5767edab5fe59a65876c0
Cassio Santana <cassioo.roger@gmail.com>
2023-01-04 23:35:31 -0300

    Wildcard types

---
+++ Generics/Wildcards001.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.text.html.StyleSheet.ListPainter;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // List<Object> is not the supertype of any list type
+        List<Object> myObjs = new ArrayList<>();
+        List<Integer> myNumbers = new ArrayList<>();
+
+        myObjs = myNumbers; // Compilation error
+
+        // The supertype of any type of list is List<?>. 
+        // <?> is a wild card
+        List<?> myObjs2 = new ArrayList<Object>();
+        List<Integer> myNumbers2 = new ArrayList<Integer>();
+        myObjs2 = myNumbers;
+    }
+}
