c41feb71ce3b5142b28bb7c0c5217cd5c3ada321
Cassio Santana <cassioo.roger@gmail.com>
2023-01-13 22:02:17 -0300

    A function as an argument

---
+++ Functional programming and lambda expressions/Example003.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Product {
+	String name;
+	Double price;
+
+	public Product(String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", price=" + price + "]";
+	}
+}
+
+public class Example003 {
+
+	public static int compareProducts(Product p1, Product p2) {
+		return p1.getPrice().compareTo(p2.getPrice());
+	}
+
+	public static void main(String[] args) {
+		
+		List<Product> list = new ArrayList<>();
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Laptop", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+		
+		/*
+		 * It is possible to pass a function as an argument 
+		 * to another function or even to return it in a function.
+		 */
+		list.sort(Example003::compareProducts);
+		list.forEach(System.out::println);
+	}
+}
