ab54bc55bca88b4c91c2df20bdd94a9c39f8ad15
Cassio Santana <cassioo.roger@gmail.com>
2023-01-25 06:43:03 -0300

    DAO JDBC - findById implementation

--- Database access with JDBC/DAO JDBC/src/application/Program.java
+++ Database access with JDBC/DAO JDBC/src/application/Program.java
@@ -1,21 +1,15 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(12, "Cassio", "cassio@gmail.com", new Date(), 5000.0, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+		Seller seller = sellerDao.findById(2);
 		System.out.println(seller);
 	}
 }
--- Database access with JDBC/DAO JDBC/src/model/dao/impl/SellerDaoJDBC.java
+++ Database access with JDBC/DAO JDBC/src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
+	
+	private Connection conn;
+	
+	public SellerDaoJDBC (Connection conn) {
+		this.conn = conn;
+	}
 
 	@Override
 	public void insert(Seller obj) {
@@ -27,8 +40,45 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE seller.Id = ?");
+			
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			
+			/*
+			 * When the query was successful, the resultSet stores the result table, 
+			 * but it points to index 0. the nex() method makes it move positions in 
+			 * this table and returns a boolean.
+			 */
+			if (rs.next()) {
+				Department department = new Department();
+				department.setId(rs.getInt("DepartmentId"));
+				department.setName(rs.getString("DepName"));
+				
+				Seller seller = new Seller();
+				seller.setId(rs.getInt("Id"));
+				seller.setName(rs.getString("Name"));
+				seller.setEmail(rs.getString("Email"));
+				seller.setBirthDate(rs.getDate("BirthDate"));
+				seller.setBaseSalary(rs.getDouble("BaseSalary"));
+				seller.setDepartment(department);
+				return seller;
+			}
+			return null;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
@@ -36,5 +86,4 @@ public class SellerDaoJDBC implements SellerDao {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
