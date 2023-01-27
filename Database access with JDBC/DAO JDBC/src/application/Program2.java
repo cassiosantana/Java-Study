d4158be4014efac245892ffd5051a579dc472cb5
Cassio Santana <cassioo.roger@gmail.com>
2023-01-27 07:15:04 -0300

    DAO JDBC - deleteById implementation in DepartmentDaoJDBC

--- Database access with JDBC/DAO JDBC/src/application/Program2.java
+++ Database access with JDBC/DAO JDBC/src/application/Program2.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -10,6 +11,7 @@ import model.entities.Department;
 public class Program2 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		Department dep = null;
@@ -19,14 +21,22 @@ public class Program2 {
 		departmentDao.insert(dep);
 		System.out.println(dep);
 
-		System.out.println("=== TEST 2: department findById ===");
+		System.out.println("\n=== TEST 2: department findById ===");
 		dep = departmentDao.findById(4);
 		System.out.println(dep);
 		
-		System.out.println("=== TEST 3: department findAll ===");
+		System.out.println("\n=== TEST 3: department findAll ===");
 		List<Department> list = new ArrayList<>();
 		list = departmentDao.findAll();
 		list.forEach(System.out::println);
+		
+		System.out.println("\n=== TEST 4: seller deleteById ===");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed!");
+		
+		sc.close();
 	}
 
 }
