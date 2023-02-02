9e945c34e0b97b913d64ca31e0aca109fa42a7ba
Cassio Santana <cassioo.roger@gmail.com>
2023-02-02 08:40:23 -0300

    Project: Web Services JPA - Hibernate: OrderStatus enum

--- Spring Boot 3 - JPA/Web Services JPA - Hibernate/src/main/java/com/cassiosantana/course/entities/Order.java
+++ Spring Boot 3 - JPA/Web Services JPA - Hibernate/src/main/java/com/cassiosantana/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.cassiosantana.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -26,6 +27,8 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -33,10 +36,19 @@ public class Order implements Serializable {
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderstatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderstatus);
 		this.client = client;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public Instant getMoment() {
@@ -46,6 +58,16 @@ public class Order implements Serializable {
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
 
 	public User getClient() {
 		return client;
@@ -53,14 +75,6 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
 	}
 
 	@Override
