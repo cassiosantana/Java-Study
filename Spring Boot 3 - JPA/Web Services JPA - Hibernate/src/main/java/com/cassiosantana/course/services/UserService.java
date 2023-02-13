9afb74872dbeadca38cd1fb7fdba175bf7e2aea7
Cassio Santana <cassioo.roger@gmail.com>
2023-02-13 15:58:50 -0300

    Project: Web Services JPA - Hibernate: Exception handling - findById

--- Spring Boot 3 - JPA/Web Services JPA - Hibernate/src/main/java/com/cassiosantana/course/services/UserService.java
+++ Spring Boot 3 - JPA/Web Services JPA - Hibernate/src/main/java/com/cassiosantana/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.cassiosantana.course.entities.User;
 import com.cassiosantana.course.repositories.UserRepository;
+import com.cassiosantana.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
