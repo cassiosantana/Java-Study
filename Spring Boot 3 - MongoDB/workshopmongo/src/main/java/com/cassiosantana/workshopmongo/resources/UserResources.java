0dee6c879fa6185ab64bed44f6033d3d33216efe
Cassio Santana <cassioo.roger@gmail.com>
2023-02-15 20:29:27 -0300

    Project: Workshop MongoDB: Usando o padrão DTO para retornar usuários

--- Spring Boot 3 - MongoDB/workshopmongo/src/main/java/com/cassiosantana/workshopmongo/resources/UserResources.java
+++ Spring Boot 3 - MongoDB/workshopmongo/src/main/java/com/cassiosantana/workshopmongo/resources/UserResources.java
@@ -1,6 +1,7 @@
 package com.cassiosantana.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cassiosantana.workshopmongo.domain.User;
+import com.cassiosantana.workshopmongo.dto.UserDTO;
 import com.cassiosantana.workshopmongo.services.UserServices;
 
 @RestController
@@ -19,9 +21,10 @@ public class UserResources {
 	private UserServices service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 }
