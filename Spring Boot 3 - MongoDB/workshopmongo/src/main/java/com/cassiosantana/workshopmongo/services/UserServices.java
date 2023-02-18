60d1fdf9f1807212fd2558580d3b5b77dcbb16af
Cassio Santana <cassioo.roger@gmail.com>
2023-02-18 18:25:05 -0300

    Project: Workshop MongoDB: Delecao de usuario com DELETE

--- Spring Boot 3 - MongoDB/workshopmongo/src/main/java/com/cassiosantana/workshopmongo/services/UserServices.java
+++ Spring Boot 3 - MongoDB/workshopmongo/src/main/java/com/cassiosantana/workshopmongo/services/UserServices.java
@@ -30,6 +30,11 @@ public class UserServices {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO userDto) {
 		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
