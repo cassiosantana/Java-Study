c531256cf215d181f8266a381a86e29d397b669c
Cassio Santana <cassioo.roger@gmail.com>
2023-02-20 07:58:36 -0300

    Project: Workshop MongoDB: Obtendo um post por id

---
+++ Spring Boot 3 - MongoDB/workshopmongo/src/main/java/com/cassiosantana/workshopmongo/resources/PostResources.java
@@ -0,0 +1,26 @@
+package com.cassiosantana.workshopmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cassiosantana.workshopmongo.domain.Post;
+import com.cassiosantana.workshopmongo.services.PostServices;
+
+@RestController
+@RequestMapping(value ="/posts" )
+public class PostResources {
+	
+	@Autowired
+	private PostServices service;
+	
+	@GetMapping(value ="/{id}" )
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
