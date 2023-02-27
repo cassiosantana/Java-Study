09f0adba85d8e0578c7ed0a219e35ee8c1a38ffd
Cassio Santana <cassioo.roger@gmail.com>
2023-02-27 09:16:35 -0300

    Workshop JavaFX JDBC: Update department

--- Interface gráfica com JavaFX/workshop-javafx-jdbc/src/gui/DepartmentFormController.java
+++ Interface gráfica com JavaFX/workshop-javafx-jdbc/src/gui/DepartmentFormController.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
-import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
