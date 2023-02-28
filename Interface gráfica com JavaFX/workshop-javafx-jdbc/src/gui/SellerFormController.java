aae3e1c9bd58b0e586c71138eaa6bd6e4fb983b0
Cassio Santana <cassioo.roger@gmail.com>
2023-02-27 22:17:25 -0300

    Workshop JavaFX JDBC: Saving Seller

--- Interface gráfica com JavaFX/workshop-javafx-jdbc/src/gui/SellerFormController.java
+++ Interface gráfica com JavaFX/workshop-javafx-jdbc/src/gui/SellerFormController.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -118,7 +120,28 @@ public class SellerFormController implements Initializable {
 		}
 
 		obj.setName(txtName.getText());
-
+		
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			exception.addError("email", "Field can't be empty");
+		}
+		
+		obj.setEmail(txtEmail.getText());
+		
+		if (dpBirthDate.getValue() == null) {
+			exception.addError("birthDate", "Field can't be empty");
+		} else {
+			Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+			obj.setBirthDate(Date.from(instant));
+		}
+		
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			exception.addError("baseSalary", "Field can't be empty");
+		}
+		
+		obj.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
+		
+		obj.setDepartment(comboBoxDepartment.getValue());
+		
 		if (exception.getErrors().size() > 0) {
 			throw exception;
 		}
@@ -187,9 +210,10 @@ public class SellerFormController implements Initializable {
 	}
 
 	public void setErrorMessages(Map<String, String> errors) {
-		if (errors.containsKey("name")) {
-			labelErrorName.setText(errors.get("name"));
-		}
+		labelErrorName.setText(errors.containsKey("name") ? errors.get("name") : "");
+		labelErrorEmail.setText(errors.containsKey("email") ? errors.get("email") : "");
+		labelErrorBirthDate.setText(errors.containsKey("birthDate") ? errors.get("birthDate") : "");
+		labelErrorBaseSalary.setText(errors.containsKey("baseSalary") ? errors.get("baseSalary") : "");
 	}
 
 	private void initializeComboBoxDepartment() {
