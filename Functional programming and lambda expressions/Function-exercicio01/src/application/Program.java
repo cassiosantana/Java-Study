ddf69aeaadf64cbfd54bfe8a3fb017c8e1988f00
Cassio Santana <cassioo.roger@gmail.com>
2023-03-06 18:12:09 -0300

    Function-exercício01 - 5 formas de usar Function

---
+++ Functional programming and lambda expressions/Function-exercicio01/src/application/Program.java
@@ -0,0 +1,23 @@
+package application;
+
+import entities.Product;
+import util.UpperCaseName;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Program {
+
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        List<String> nameList = list.stream().map(new UpperCaseName()).toList();
+        nameList.forEach(System.out::println);
+    }
+
+}
---
+++ Functional programming and lambda expressions/Function-exercicio01/src/application/Program4.java
@@ -0,0 +1,23 @@
+package application;
+
+import entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class Program4 {
+
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        Function<Product, String> function = product -> product.getName().toUpperCase();
+        List<String> nameList = list.stream().map(function).toList();
+        nameList.forEach(System.out::println);
+    }
+
+}
