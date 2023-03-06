63691a6d24877cd1658f53dabc72db945427575d
Cassio Santana <cassioo.roger@gmail.com>
2023-03-06 16:25:18 -0300

    Predicate-exercício01 - Expressão lambda inline

---
+++ Functional programming and lambda expressions/Predicate-exercicio01/src/application/Program5.java
@@ -0,0 +1,23 @@
+package application;
+
+import entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Program5 {
+
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        double precoMaximo = 100.0;
+        list.removeIf(product -> product.getPrice() > precoMaximo);
+        list.forEach(System.out::println);
+    }
+
+}
